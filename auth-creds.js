6855e42d445afc3344cd2399b77001af5a09bd13
Joseph Potapenko <129787506+JosephPotapenko@users.noreply.github.com>
2025-08-21 20:53:54 -0700

    Implement login functionality with client-side cooldown and error handling

---
+++ auth-creds.js
@@ -0,0 +1,8 @@
+// Credentials for change-management login (obfuscated, not secure)
+window.changeMgmtAuth = {
+  user: 'joepotap@gmail.com',
+  pass: '12341234'
+};
+//Good job for finding the totally unsecure login credentials for a websight that has no real value! You must be so proud of yourself!
+//This was never meant to be truly secure considering no actual sensitive data is involved. This was just a deterant from people who act dumb. Thank-you for your time and consideration when it came to locating this!
+//It really means a lot when someone actually enjoys doing this kind of stuff for fun!
